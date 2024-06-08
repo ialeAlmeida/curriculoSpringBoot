d8d4925973c1d8fd17215829e8341e1cfa560fb8
ialeAlmeida <almeidaialesantos@gmail.com>
2024-06-08 09:57:18 -0300

    added: id em Pessoa.java

--- src/main/java/com/br/pessoal/curriculoSpringBoot/model/Pessoa.java
+++ src/main/java/com/br/pessoal/curriculoSpringBoot/model/Pessoa.java
@@ -1,6 +1,9 @@
 package com.br.pessoal.curriculoSpringBoot.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,6 +15,9 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String nome;
     private int idade;
     private String cargoAlmejado;
