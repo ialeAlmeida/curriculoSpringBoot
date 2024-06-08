e296b2421f5378321cd20291e9e2d18117af537b
ialeAlmeida <almeidaialesantos@gmail.com>
2024-06-08 09:36:15 -0300

    added: estrutura projeto spring boot com maven

---
+++ src/main/java/com/br/pessoal/curriculoSpringBoot/CurriculoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.br.pessoal.curriculoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurriculoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/pessoal/curriculoSpringBoot/CurriculoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.pessoal.curriculoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
